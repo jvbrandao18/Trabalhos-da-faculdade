8c469c60c37fefec5589d72db5d495a2495a269a
Brandao <66882890+jvbrandao18@users.noreply.github.com>
2022-10-14 09:58:19 -0300

    Create trabalho9.clj

--- Frank/Clojure/trabalho9.clj
+++ Frank/Clojure/trabalho9.clj
@@ -10,7 +10,6 @@
   (if (= (count lista) 1)
     (first lista)
     (ultimo (rest lista))))
-(println (ultimo [1 2 3 4 5 6 7 8 9 10]))
 
 ;; 2. Utilizando a linguagem Clojure, crie uma função chamada penultimo que receba uma lista
 ;; e devolva o penúltimo elemento desta lista usar as funções já prontas e disponíveis para
@@ -20,7 +19,6 @@
   (if (= (count lista) 2)
     (first lista)
     (penultimo (rest lista))))
-(println (penultimo [1 2 3 4 5 6 7 8 9 10]))
 
 ;; 3. Utilizando a linguagem Clojure, crie uma função chamada elementoN que receba uma lista 
 ;; e um inteiro N e devolva o  elemento que  está na  posição N desta lista usar as funções já 
@@ -30,19 +28,18 @@
   (if (= n 1)
     (first lista)
     (elementoN (rest lista) (- n 1))))
-(println (elementoN [1 2 3 4 5 6 7 8 9 10] 5))
-
 
 ;; 4. Utilizando  a  linguagem Clojure,  crie  uma função  chamada  inverso  que  receba uma  lista  e 
 ;; devolva esta lista com as posições dos elementos invertidas. Por exemplo recebe [1,2,3] e 
 ;; devolve [3,2,1]. Sem usar as funções já prontas e disponíveis para esta mesma finalidade na 
 ;; linguagem Clojure.  
 
-(defn inverso [lista]
-  (if (= (count lista) 1)
-    lista
-    (conj (inverso (rest lista)) (first lista))))
-(println (inverso [1 2 3 4 5 6 7 8 9 10]))
+(defn inverso
+  ([[fst & rst]]
+   (if (empty? rst)
+    (list fst)
+    (concat (inverso rst) (list fst))))
+)
 
 ;; 5. Utilizando a  linguagem Clojure, crie uma função chamada  mdc que receba  dois inteiros e 
 ;; devolve o mínimo divisor comum entre eles.  Sem usar as funções já prontas e disponíveis 
@@ -52,4 +49,21 @@
   (if (= b 0)
     a
     (mdc b (mod a b))))
-(println (mdc 10 5))
+
+
+;; Chamada das funções
+(println "-------------------------------------------------------------")
+(println "ultimo: entrada: [2 4 6 12]; resultado: " (ultimo [2 4 6 12]))
+(println "ultimo: entrada: [12 6 8 2 5]; resultado: " (ultimo [12 6 8 2 5]))
+(println "-------------------------------------------------------------")
+(println "penultimo: entrada: [1 5 10 24]; resultado: " (penultimo [1 5 10 24]))
+(println "penultimo: entrada: [24 10 8 2 5]; resultado: " (penultimo [24 10 8 2 5]))
+(println "-------------------------------------------------------------")
+(println "elementoN: entrada: [2 4 6 12] 4; resultado: "(elementoN [2 4 6 12] 3))
+(println "elementoN: entrada: [12 6 8 2 5] 2; resultado: "(elementoN [12 6 8 2 5] 2))
+(println "-------------------------------------------------------------")
+(println "inverso: entrada: [2 4 6 12]; resultado: " (inverso [2 4 6 12]))
+(println "inverso: entrada: [24 10 8 2 5]; resultado: " (inverso [24 10 8 2 5]))
+(println "-------------------------------------------------------------")
+(println "mdc: entrada: 10 5; resultado: " (mdc 10 5))
+(println "mdc: entrada: 15 30; resultado: " (mdc 15 30))
