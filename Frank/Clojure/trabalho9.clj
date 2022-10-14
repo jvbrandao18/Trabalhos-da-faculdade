d8cb88bc1c377f722d9a30fdb1b53951de8e220f
Brandao <66882890+jvbrandao18@users.noreply.github.com>
2022-10-14 10:01:07 -0300

    Update trabalho9.clj

--- Frank/Clojure/trabalho9.clj
+++ Frank/Clojure/trabalho9.clj
@@ -38,8 +38,7 @@
   ([[fst & rst]]
    (if (empty? rst)
     (list fst)
-    (concat (inverso rst) (list fst))))
-)
+    (concat (inverso rst) (list fst)))))
 
 ;; 5. Utilizando a  linguagem Clojure, crie uma função chamada  mdc que receba  dois inteiros e 
 ;; devolve o mínimo divisor comum entre eles.  Sem usar as funções já prontas e disponíveis 
